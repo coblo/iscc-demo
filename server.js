922dfc1f946a9b6521fe9348e4d42e06b0793d14
Patricia Schinke <schinke@creations.de>
2017-07-06 16:28:24 +0200

    add jaccard similarity to log

--- server.js
+++ server.js
@@ -94,33 +94,63 @@ io.on('connection', socket => {
 					json: true
 				}).then(dataInstanceResponse => {
 
-					let lastBits = false;
-					if (logEntries.length > 0)
-						lastBits = logEntries[logEntries.length - 1].iscc;
+					let differences;
+					if (logEntries.length > 0) {
+						let lastBits = logEntries[logEntries.length - 1].iscc;
+						differences = {
+							meta_id: diffArray(metaResponse.meta_id.bits, lastBits.metaID.bits ),
+							content_id: diffArray(textResponse.content_id.bits, lastBits.contentID.bits ),
+							data_id: diffArray(dataInstanceResponse.data_id.bits, lastBits.dataID.bits ),
+							instance_id: diffArray(dataInstanceResponse.instance_id.bits, lastBits.instanceID.bits )
+						};
+					}
+					else
+						differences = {
+							meta_id: diffArray(metaResponse.meta_id.bits, metaResponse.meta_id.bits),
+							content_id: diffArray(textResponse.content_id.bits, textResponse.content_id.bits),
+							data_id: diffArray(dataInstanceResponse.data_id.bits, dataInstanceResponse.data_id.bits),
+							instance_id: diffArray(dataInstanceResponse.instance_id.bits, dataInstanceResponse.instance_id.bits)
+						};
+					let similarity = {
+						meta_id: jaccard_sim(differences.meta_id),
+						content_id: jaccard_sim(differences.content_id),
+						data_id: jaccard_sim(differences.data_id),
+						instance_id: jaccard_sim(differences.instance_id)
+					};
 					let result = {
 						metaID: {
 							'code': metaResponse.meta_id.code,
 							'bits': metaResponse.meta_id.bits,
-							'diff': diffArray(metaResponse.meta_id.bits, lastBits ? lastBits.metaID.bits : metaResponse.meta_id.bits)
+							'diff': differences.meta_id,
+							'sim': similarity.meta_id
 						},
 						contentID: {
 							'code': textResponse.content_id.code,
 							'bits': textResponse.content_id.bits,
-							'diff': diffArray(textResponse.content_id.bits, lastBits ? lastBits.contentID.bits: textResponse.content_id.bits)
+							'diff': differences.content_id,
+							'sim': similarity.content_id
 						},
 						dataID: {
 							'code': dataInstanceResponse.data_id.code,
 							'bits': dataInstanceResponse.data_id.bits,
-							'diff': diffArray(dataInstanceResponse.data_id.bits, lastBits ? lastBits.dataID.bits : dataInstanceResponse.data_id.bits)
+							'diff': differences.data_id,
+							'sim': similarity.data_id
 						},
 						instanceID: {
 							'code': dataInstanceResponse.instance_id.code,
 							'bits': dataInstanceResponse.instance_id.bits,
-							'diff': diffArray(dataInstanceResponse.instance_id.bits, lastBits ? lastBits.instanceID.bits : dataInstanceResponse.instance_id.bits)
+							'diff': differences.instance_id,
+							'sim': similarity.instance_id
 						}
 					};
 
+					let d = new Date();
+
+					let timeStamp = d.getFullYear() + '-' + (d.getMonth() < 9 ? '0' : '') + (d.getMonth() + 1) + '-' +
+						(d.getDate() < 10 ? '0' : '') + d.getDate() + ' ' + d.toTimeString().substring(0, 8);
+
 					let entry = {
+						time: timeStamp,
 						iscc: result,
 						title: data.title,
 						creator: data.creators,
@@ -152,6 +182,11 @@ app.get('/', function (req, res) {
 	renderPage(res, 'main', {logEntries: logEntries});
 
 });
+
+function jaccard_sim(bool_array) {
+	let true_count = bool_array.reduce(function(a,b){ return b?a+1:a; },0);
+	return Math.round(true_count / (2 * bool_array.length - true_count) * 100);
+}
 
 function diffArray(arr1, arr2) {
 	let diff = [];
